2a8b8062ec5f359b6d8ec30d4af48b00c133f6a3
dassiorleando <me@dassiorleando.xyz>
2017-03-09 15:47:35 +0100

    Integrate Bootstrap for a beautifull UI

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'devise'
 
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'site/index'
+
   devise_for :users
   resources :bookmarks
   # root to: "home#index"
-  root "bookmarks#index"
+  # root "bookmarks#index"
+  root 'site#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
